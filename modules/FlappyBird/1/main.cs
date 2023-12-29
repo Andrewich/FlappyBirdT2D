9e8f66cb6662de8ea63315ffee9295ff4dd799fe
Egorov Maxim <abd.andrew@gmail.com>
2023-12-29 10:21:03 +0500

    Add move cycle Pipes.
    Collect all object in Scene.taml.
    Collect Pipes in SimSet.

--- modules/FlappyBird/1/main.cs
+++ modules/FlappyBird/1/main.cs
@@ -27,82 +27,14 @@ function FlappyBird::loadGame(%this)
 
    %scene = TamlRead("./objects/Scene.taml");
    %window.setScene(%scene);   
-
-   //%background = TamlRead("./objects/Background.taml");
-   //%scene.add(%background);
-   
-   //%ground = TamlRead("./objects/Ground.taml");
-   //%scene.add(%ground);
-   
-   //%bird = TamlRead("./objects/Bird.taml");   
-   //%scene.add(%bird);
-   
-   //%pipe = new Sprite() {
-      //class = "Pipe";
-      //SceneLayer = 1;
-      //Position = "12.5 -5";
-      //Size = "2 8.75";
-      //BodyType = "Kinematic";        
-      //Image = "FlappyBird:Pipe";
-      //Frame = 0;         
-   //};
-   //%scene.add(%pipe);
-   //%pipe.moveTo("-12.5 -5", 5);
    
    %pipes = TamlRead("./objects/Pipes.taml");   
-   //%pipes.listObjects();   
    for (%index = 0; %index < %pipes.getCount(); %index++)
    {
       %pipe = %pipes.getObject(%index);
       %scene.add(%pipe);
       %pipe.moveToEnd();
    }
-   
-   //%pipes = new SimSet();
-   //for (%count = 0; %count < 3; %count++)
-   //{
-      //%pipe = new Sprite() {
-         //class = "Pipe";
-         //origin = "14.5 -5";
-         //SceneLayer = 1;
-         //Size = "2 8.75";
-         //BodyType = "Kinematic";
-         //Image = "FlappyBird:Pipe";
-         //Frame = 0;
-      //};
-      //%pipe.createPolygonBoxCollisionShape();
-      //%pipe.setPosition(14.5 + (%count * 10), -5 );      
-      //%scene.add(%pipe);      
-      //%pipe.moveToEnd();
-      //%pipes.add(%pipe);
-   //}
-   //TamlWrite(%pipes, "stuff.taml");
-      
-   
-   //TamlWrite( %scene, "stuff.taml");
-   
-   //for (%count = 0; %count < 4; %count++)
-   //{
-      //%pipes[%count] = new Sprite() {
-         //class = "Pipe";
-         //SceneLayer = 1;
-         //Size = "2 8.75";
-         //BodyType = "Kinematic";
-         ////LinearVelocity = "-5 0";
-         //Image = "FlappyBird:Pipe";
-         //Frame = 0;         
-      //};
-      ////%pipes[%count].createPolygonBoxCollisionShape();
-      //%pos_x = 14.5 + (%count * 9);
-      //%pos_y = -5;
-      //%pipes[%count].setPosition(%pos_x, %pos_y);      
-      //%scene.add(%pipes[%count]);
-   //}
-   
-   //%pipe = TamlRead("./objects/Pipe.taml");
-   //%scene.add(%pipe);   
-   //TamlWrite( %pipe, "stuff.taml" );
-   //TamlWrite(%scene, "stuff.taml");
    
    %scene.setDebugOn( "collision" );
 }
